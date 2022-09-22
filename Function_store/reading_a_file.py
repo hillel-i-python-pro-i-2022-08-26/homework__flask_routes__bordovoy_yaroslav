1545813de8e3ec3d7aea54ac40c231f74380c3b9
Yaroslav <superbobs777@gmail.com>
2022-09-22 22:44:37 +0300

    Correct function name

--- Function_store/reading_a_file.py
+++ Function_store/reading_a_file.py
@@ -11,11 +11,11 @@ ROOT_PATH = pathlib.Path(__file__).parents[1]
 FILES_PATH = ROOT_PATH.joinpath("requirements.txt")
 
 
-def read():
+def read_requirements():
 
     with open(FILES_PATH, 'r') as file:
         return file.read()
 
 
 if __name__ == "__main__":
-    read()
+    read_requirements()
