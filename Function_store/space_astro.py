46533db2843fc890d6339911a55b65b012081444
Yaroslav <superbobs777@gmail.com>
2022-09-23 19:07:01 +0300

    Add "space_astro.py" file

---
+++ Function_store/space_astro.py
@@ -0,0 +1,24 @@
+"""
+Вывести количество космонавтов в настоящий момент.
+Можно вывести как минимум просто цифру с результатом. Можно добавить ещё
+текст, описывающий смысл этой цифры.
+При желании, можно также вывести дополнительную информацию.
+Что явно запрещено, так это просто выводить полученный JSON. Нужно его
+обработать, взять нужные данные и передать на вывод.
+"""
+import json
+
+import requests
+
+
+def get_astro():
+    url = "http://api.open-notify.org/astros.json"
+    response = requests.get(url)
+    text_json = response.text
+    ready_json = json.loads(text_json)
+
+    return ready_json
+
+
+if __name__ == "__main__":
+    get_astro()
