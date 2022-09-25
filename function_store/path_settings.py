6c1ff66f95aea356676dfb3e80ed1c72370227dd
Yaroslav <superbobs777@gmail.com>
2022-09-25 10:28:10 +0300

    Functions removed, only paths left

--- function_store/path_settings.py
+++ function_store/path_settings.py
@@ -1,15 +1,5 @@
 import pathlib
 
 
-def read_txt():
-    ROOT_PATH = pathlib.Path(__file__).parents[1]
-    FILES_PATH_txt = ROOT_PATH.joinpath("requirements.txt")
-
-    return FILES_PATH_txt
-
-
-def read_csv():
-    ROOT_PATH = pathlib.Path(__file__).parents[0]
-    FILES_PATH_csv = ROOT_PATH.joinpath("people_data.csv")
-
-    return FILES_PATH_csv
+ROOT_PATH_txt = pathlib.Path(__file__).parents[1]
+ROOT_PATH_csv = pathlib.Path(__file__).parents[0]
