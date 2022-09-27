990645ba10d66dd715f86498855543cd7a42ef30
Yaroslav <superbobs777@gmail.com>
2022-09-27 23:14:22 +0300

    Add new path

--- function_store/path_settings.py
+++ function_store/path_settings.py
@@ -1,8 +1,12 @@
 import pathlib
 
 
-ROOT_PATH_txt = pathlib.Path(__file__).parents[1]
-FILES_PATH_txt = ROOT_PATH_txt.joinpath("requirements.txt")
+ROOT_PATH = pathlib.Path(__file__).parents[1]
+FILES_PATH_txt = ROOT_PATH.joinpath("requirements.txt")
 
-ROOT_PATH_csv = pathlib.Path(__file__).parents[0]
+ROOT_PATH_csv = pathlib.Path(
+    "/home/yaros/homework__flask_routes__bordovoy_yaroslav/function_store/flask_foo/"
+)
 FILES_PATH_csv = ROOT_PATH_csv.joinpath("people_data.csv")
+
+DB_PATH = ROOT_PATH.joinpath("database_sql", "db.sqlite")
