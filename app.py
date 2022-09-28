3c28b7c79faff1b3fa610763379dbb58c98d5288
Yaroslav <superbobs777@gmail.com>
2022-09-28 22:36:23 +0300

    Add routes for DB

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, Response
+from webargs import fields
+from webargs.flaskparser import use_args
 
-from function_store import reading_a_file, user_generator, space_astro, average_data
+from function_store.db_foo.creating_sql_table import create_table
+from function_store.db_foo.db_connection import DBConnection
+from function_store.flask_foo import (
+    average_data,
+    space_astro,
+    user_generator,
+    reading_a_file,
+)
 
 app = Flask(__name__)
 
@@ -32,27 +41,108 @@ def generate_users(amount: int = 100) -> str:
 @app.route("/space")
 def space() -> str:
     num_astro = space_astro.get_astro()
-    iss_list = []
-    ship_list = []
-    for i in num_astro["people"]:
-        for v in i.values():
-            if v == "ISS":
-                iss_list.append(i["name"])
-            elif v == "Tiangong":
-                ship_list.append(i["name"])
-            else:
-                "This ship is not in the database"
-    return (
-        f'<p>Astronauts at the moment: {num_astro["number"]}</p>'
-        f'<p>ISS crafting: {", ".join(iss_list)}</p>'
-        f'<p>Tiangong crafting: {", ".join(ship_list)}</p>'
-    )
+    yield f'<p>Astronauts at the moment: {num_astro["number"]}</p><br>'
+    for name in num_astro["people"]:
+        yield f'<p>{name["name"]} is part of the {name["craft"]} group</p>'
 
 
 # PATH: /mean/
 @app.route("/mean")
 def mean() -> str:
     return f"{average_data.average()}"
+
+
+# Database SQlite
+create_table()
+
+
+@app.route("/phones/create")
+@use_args(
+    {
+        "contact_name": fields.Str(required=True),
+        "phone_value": fields.Int(required=True),
+    },
+    location="query",
+)
+def db_phones_create(args):
+    with DBConnection() as connection:
+        with connection:
+            connection.execute(
+                "INSERT INTO phones (contact_name, phone_value) VALUES (:contact_name, :phone_value)",
+                {
+                    "contact_name": args["contact_name"],
+                    "phone_value": args["phone_value"],
+                },
+            )
+
+    return "Data entered"
+
+
+@app.route("/phones/read-all")
+def db_phones_read_all():
+    with DBConnection() as connection:
+        names_from_table = connection.execute("SELECT * FROM phones;").fetchall()
+
+    return "<br>".join(
+        [
+            f"{names['phone_ID']}: {names['contact_name']} --- {names['phone_value']}"
+            for names in names_from_table
+        ]
+    )
+
+
+@app.route("/phones/read-user/<int:phone_ID>")
+def db_phones_read_user(phone_ID: int):
+    with DBConnection() as connection:
+        user = connection.execute(
+            "SELECT * FROM phones WHERE (phone_ID=:phone_ID);",
+            {"phone_ID": phone_ID},
+        ).fetchone()
+
+        if user is None:
+            return f"Sorry, user ID {phone_ID} does not exist"
+
+    return f'{user["phone_ID"]}: {user["contact_name"]} - {user["phone_value"]}'
+
+
+@app.route("/phones/update/<int:phone_ID>")
+@use_args({"contact_name": fields.Str(), "phone_value": fields.Int()}, location="query")
+def db_phones_update(args, phone_ID: int):
+    with DBConnection() as connection:
+        with connection:
+            contact_name = args.get("contact_name")
+            phone_value = args.get("phone_value")
+            if contact_name is None and phone_value is None:
+                return Response("Sorry, you dont provide any value", status=400)
+
+            args_for_request = []
+            if contact_name is not None:
+                args_for_request.append("contact_name=:contact_name")
+            if phone_value is not None:
+                args_for_request.append("phone_value=:phone_value")
+
+            args_str = ", ".join(args_for_request)
+
+            connection.execute(
+                f"UPDATE phones SET {args_str} WHERE (phone_ID=:phone_ID);",
+                {
+                    "phone_ID": phone_ID,
+                    "contact_name": contact_name,
+                    "phone_value": phone_value,
+                },
+            )
+    return f"User ID {phone_ID} data changed"
+
+
+@app.route("/phones/delete/<int:phone_ID>")
+def db_phones_delete(phone_ID):
+    with DBConnection() as connection:
+        with connection:
+            connection.execute(
+                "DELETE FROM phones WHERE (phone_ID=:phone_ID);", {"phone_ID": phone_ID}
+            )
+
+    return f"User ID {phone_ID} deleted"
 
 
 if __name__ == "__main__":
